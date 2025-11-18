8b45835dce9cb4cdeaa55bd640f6cd13f96a4c70
Keyurkarenai2 <karenakeyur20@gmail.com>
2025-11-18 10:07:00 +0530

    route_testing

--- routes/auth.js
+++ routes/auth.js
@@ -28,7 +28,7 @@ router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', verifyOtpAndReset);
 
 router.get('/google-login-test', (req, res) => {
-  res.json({ message: 'Google login route is working!' });
+  res.json({ message: 'Google login route is working done!' });
 });
 
 router.post('/google-login', googleLogin);
